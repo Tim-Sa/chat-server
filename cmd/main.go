99a8545f4bdc749a7ea9361a9f5327e3aea93f4c
Tim-Sa <salnikovtimofeyof@gmail.com>
2024-02-21 12:55:06 +0300

    base chat-server grc-schema

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,37 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net"
+
+	"github.com/fatih/color"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	desc "github.com/Tim-Sa/chat-server/pkg/chat_v1"
 )
 
+const grpcPort = 50051
+
+type server struct {
+	desc.UnimplementedChatV1Server
+}
+
 func main() {
-	fmt.Println("Starting chat-server..")
+	fmt.Println(color.GreenString("Chat service start"))
+
+	listen, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterChatV1Server(s, &server{})
+
+	log.Printf("server listening at %v", listen.Addr())
+
+	if err = s.Serve(listen); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
